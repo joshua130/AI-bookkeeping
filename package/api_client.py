d9bb0f46c91215c7e414239453f246ea33fc4ef8
joshua130 <genjoshua130@gmail.com>
2026-03-17 23:03:20 +0900

    内部処理の土台を作成 events_watchでファイルの更新を監視 analysisで解析 convertでTKCの仕訳へ変換する

--- package/api_client.py
+++ package/api_client.py
@@ -1,4 +1,3 @@
 from google import genai
 
 client = genai.Client()
-    
---
+++ servise/analysis.py
@@ -0,0 +1,9 @@
+from google import genai
+from pydantic import BaseModel
+from typing import List
+
+class tkc_json(BaseModel):
+    #これをしっかり定義する必要がある
+    pass
+
+from package.api_client import client
