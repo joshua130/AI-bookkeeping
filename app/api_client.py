b9694a85b0ede9d033bccd963a1d4dcdc0f57a1c
joshua130 <genjoshua130@gmail.com>
2026-03-17 20:45:11 +0900

    gemini_clientをセットアップしました

---
+++ app/api_client.py
@@ -0,0 +1,10 @@
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
