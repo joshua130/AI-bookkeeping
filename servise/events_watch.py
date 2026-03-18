0cb5a57164ddd8b5c3869767ca3e4403d609b90f
joshua130 <genjoshua130@gmail.com>
2026-03-18 21:29:43 +0900

    events_watchを作成中です。

--- servise/events_watch.py
+++ servise/events_watch.py
@@ -1,1 +1,9 @@
-    
+import time 
+import os
+from dotenv import load_dotenv
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+load_dotenv()
+WATCH_DIR = os.getenv("WATCH_DIR")
